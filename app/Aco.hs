574ebeea01730a12c32d5708ab6ebb5dd8d8f3cc
gianzav <gianlucagianluca18@gmail.com>
2025-05-31 20:57:46 +0200

    fixed removeLoops code, refactoring

--- app/Aco.hs
+++ app/Aco.hs
@@ -5,10 +5,10 @@ import Data.Graph (Graph, Vertex, Edge, edges)
 import qualified Data.Map.Strict as M
 import Data.Array
 import Control.Monad.Random
-
--- Generic label
-newtype Label a = Label a
-    deriving (Eq, Show)
+import Data.Tuple (swap)
+import GHC.Stack (HasCallStack)
+import qualified Data.Set as Set
+import Debug.Trace (trace)
 
 data Direction = Forward | Backward
     deriving (Eq,Show)
@@ -19,38 +19,85 @@ type Path = [Vertex]
 
 data Ant = Ant
     {
-        position :: Vertex,
-        direction :: Direction,
-        path :: Path,
-        source :: Source,
-        destination :: Destination,
-        randomGen :: StdGen
+        antPosition :: Vertex,
+        antDirection :: Direction,
+        antPath :: Path,
+        antSrc :: Source,
+        antDest :: Destination,
+        antRnd :: StdGen
     }
     deriving (Eq, Show)
 
 type Pheromone = Float              -- pheromone left by Ants
-type Volatility = Float             -- pheromone volatility
+type Volatility = Float             -- pheromone volatility rate
 
-type AntVertexLabel = Label [Ant]   -- label for vertices, a collection of ants
-type AntEdgeLabel = Label Pheromone -- label for edges, with the total deposited pheromone
+type AntVertexLabel = [Ant]         -- label for vertices, a collection of ants
+type AntEdgeLabel = Pheromone       -- label for edges, with the total deposited pheromone
 
+data LabeledGraph a b = LG
+    {
+        lgGraph :: Graph,
+        lgVertexLabels :: M.Map Vertex a,
+        lgEdgeLabels :: M.Map Edge b
+    }
+    deriving (Show, Eq)
 
+type AntGraph = LabeledGraph AntVertexLabel AntEdgeLabel
 type EdgeLabels = M.Map Edge AntEdgeLabel
 
-getEdgeLabel :: Edge -> EdgeLabels -> AntEdgeLabel
-getEdgeLabel e m = case M.lookup e m of -- calls error if the edge is not present
+getEdgeLabel :: HasCallStack => Edge -> EdgeLabels -> AntEdgeLabel
+getEdgeLabel e m = case M.lookup e m of
                     (Just label) -> label
-                    Nothing -> error ("Edge label of" ++ show e ++ " not found.")
+                    Nothing -> error ("Edge label of " ++ show e ++ " not found.")
 
 setEdgeLabel :: Edge -> AntEdgeLabel -> EdgeLabels -> EdgeLabels
 setEdgeLabel = M.insert
 
-
 neighbors :: Graph -> Vertex -> [Vertex]
 neighbors g v = g ! v -- a Graph is an Array Vertex [Vertex]
 
+-- Outgoing edges of a vertex.
 outEdges :: Graph -> Vertex -> [Edge]
-outEdges g v = map (\u -> (v,u))  (neighbors g v)
+outEdges g v = [(v,u) | u <- neighbors g v]
+
+-- Extract randomly an element from a list, where each possible choice has a weight.
+-- The higher the weight, the higher the probability of an element to be extracted.
+weightedRandomChoice :: RandomGen g => g -> [(a, Rational)] -> (g,a)
+weightedRandomChoice randGen weights = swap $ runRand m randGen
+  where m = fromList weights
+
+-- Gives the weighted list of possible next vertices for an Ant, given the
+-- Pheromone on the Edge(s).
+possibleNextVertices :: [Edge] -> EdgeLabels -> [(Vertex, Pheromone)]
+possibleNextVertices es labels = map getWeightedVertex es
+  where
+    getWeightedVertex :: Edge -> (Vertex, Pheromone)
+    getWeightedVertex e@(_,v) = let p = getEdgeLabel e labels
+                                in (v, p)
+
+-- Choose randomly a next vertex based on the pheromone on the edges
+nextVertex :: RandomGen g  => g -> [Edge] -> EdgeLabels -> (g, Vertex)
+nextVertex randGen es edgeLabels =
+    -- convert pheromone level to Rational
+    let possibleNext = [(x, toRational p) | (x,p) <- possibleNextVertices es edgeLabels]
+    in weightedRandomChoice randGen possibleNext
+
+-- Remove all loops from a path
+removeLoops :: (Ord a, Eq a) => [a] -> [a]
+removeLoops = go Set.empty []
+  where
+    go _ loopFree [] = loopFree
+    go seen loopFree (x:xs)
+      | x `Set.member` seen =
+          let newloopFree = dropWhile (/= x) (reverse loopFree)
+              newSeen = Set.fromList newloopFree
+          in go newSeen (reverse newloopFree) xs
+      | otherwise = go (Set.insert x seen) (loopFree ++ [x]) xs
+
+
+-- Positive or negative Pheromone change on an Edge
+data PheromoneUpdate = PU Edge Pheromone
+    deriving (Show, Eq)
 
 {-
  - All the Ants take atomically a step towards their direction.
@@ -59,108 +106,83 @@ outEdges g v = map (\u -> (v,u))  (neighbors g v)
  - The choice of the Edge to follow during the Ant movement is influenced by the Pheromone on the neighbors of the current Vertex.
  - When an Ant going Backward reaches the Source, its Direction becomes Backward.
 -}
--- takeStep :: Graph -> VertexLabels -> VertexLabels
-
-weightedRandomChoice :: RandomGen g => g -> [(a, Rational)] -> (g,a)
-weightedRandomChoice randGen weights = (g,a)
-    where m = fromList weights
-        -- this is not needed but otherwise I need to change all the functions using weightedRandomChoice
-          (a,g) = runRand m randGen
-
--- Gives the weighted list of possible next vertices for an Ant, given the
--- Pheromone on the Edge(s)
-nextVertices :: [Edge] -> EdgeLabels -> [(Vertex, Pheromone)]
-nextVertices es labels = map getWeightedVertex es
-    where
-    getWeightedVertex :: Edge -> (Vertex, Pheromone)
-    getWeightedVertex e@(_,v) = let (Label p) = getEdgeLabel e labels
-                                in (v, p)
-
-removeLoops :: Path -> Path
-removeLoops p = rL M.empty (zipWith (,) p [1..]) []
-    where
-    rL :: M.Map Vertex Int -> [(Vertex, Int)] -> Path -> Path
-    rL _ [] loopFree = loopFree
-    rL m ((v,pos):xs) loopFree =
-        case M.lookup v m of
-            -- if the element was found before, drop all elements in loopFree after pos'
-            (Just pos') ->
-                let loopFree' = take pos' loopFree
-                    -- need to recompute the indices
-                    m' = foldr (\(x,p) m'' -> M.insert x p m'') M.empty (zipWith (,) loopFree' [1..])
-                in rL m' xs loopFree'
-            -- otherwise, add the current element to the accumulator
-            Nothing -> rL (M.insert v pos m) xs (loopFree ++ [v])
-
-
-nextVertex :: RandomGen g  => g -> [Edge] -> EdgeLabels -> (g, Vertex)
-nextVertex randGen es edgeLabels =
-    -- convert pheromone level to Rational
-    let possibleNext = map (\(x,p) -> (x, toRational p)) (nextVertices es edgeLabels)
-    in weightedRandomChoice randGen possibleNext
-
--- Positive or negative Pheromone change on an Edge
-data PheromoneUpdate = PU Edge Pheromone
-
-
-antStep :: Graph -> EdgeLabels -> Ant -> Pheromone -> (Ant, PheromoneUpdate)
-antStep g edgeLabels a@(Ant v Forward path s d randGen) pheromone =
+antStep :: AntGraph -> Ant -> Pheromone -> (Ant, PheromoneUpdate)
+antStep (LG g _ edgeLabels) (Ant v Forward path src dst randGen) _ =
     -- don't consider the last visited Vertex
-    let butLastVisited = case path of
-                            [] -> outEdges g v
-                            _ -> case filter (\(_,y) -> y /= last path) (outEdges g v) of
-                                [] -> [(v, last path)] -- take the last edge only if there is no other option
-                                x -> x
-        possibleEdges = butLastVisited
+    let possibleEdges = case path of
+                        -- no step has been taken, all neighbors are feasible
+                        [] -> outEdges g v
+                        _ -> case filter (\(_,y) -> y /= last path) (outEdges g v) of
+                            -- take the last traversed edge only if there is no other option
+                            [] -> [(v, last path)]
+                            -- otherwise use the outgoing edges, minus the last traversed one
+                            x -> x
         (newRandGen, nxt) = nextVertex randGen possibleEdges edgeLabels
-        in if nxt == d then
-           (Ant nxt Backward (removeLoops (path ++ [v])) s d newRandGen, PU (v,nxt) 0)
-          else
-           (Ant nxt Forward (path ++ [v]) s d newRandGen, PU (v,nxt) 0)
-antStep _ _ a@(Ant v Backward path s d randGen) pheromone =
-    -- don't consider the last visited Vertex
+    in if nxt == dst then
+        -- When the destination vertex is reached, remove the loops from the traversed path
+        -- and prepare to move backwards
+        (Ant nxt Backward (removeLoops (path ++ [v])) src dst newRandGen, PU (v,nxt) 0)
+       else
+        (Ant nxt Forward (path ++ [v]) src dst newRandGen, PU (v,nxt) 0)
+antStep _ a@(Ant v Backward path src dst randGen) pheromone =
+    -- Moving backwards, the next step is the last in the forward path
     let nxt = case path of
                 [] -> error ("No vertices in path of ant " ++ show a)
                 _ -> last path
-    in if nxt == s then
-        (Ant nxt Forward [] s d randGen, PU (v,nxt) pheromone)
+    in if nxt == src then
+        -- When the source vertex is reached, start exploring again
+        -- Moving backwards, leave the pheromone on the edge
+        (Ant nxt Forward [] src dst randGen, PU (v,nxt) pheromone)
        else
-        (Ant nxt Backward (init path) s d randGen, PU (v,nxt) pheromone)
+        -- Otherwise, keep going back towards the source and leave pheromone
+        (Ant nxt Backward (init path) src dst randGen, PU (v,nxt) pheromone)
 
-graphStep :: Graph -> [Ant] -> EdgeLabels -> Pheromone -> ([Ant], EdgeLabels)
-graphStep  g [] edgeLabels _ = ([], edgeLabels)
-graphStep  g (ant:ants) edgeLabels pheromone = (finalAnts, updatedEdgeLabels)
-    where (newAnts, newEdgeLabels) = graphStep g ants edgeLabels pheromone
-          (newAnt, update) = antStep g edgeLabels ant pheromone
-          finalAnts = newAnt:newAnts
-          updatedEdgeLabels = updateEdgeLabel update newEdgeLabels
+-- Let all the ants on the graph take a step
+graphStep :: AntGraph -> [Ant] -> Pheromone -> ([Ant], AntGraph)
+graphStep g ants phero = graphStep' g ants phero []
 
-evaporate :: EdgeLabels -> Volatility -> EdgeLabels
-evaporate edgeLabels v = M.map (\(Label p) -> Label (p - (p * v))) edgeLabels
+graphStep' :: AntGraph -> [Ant] -> Pheromone -> [Ant] -> ([Ant], AntGraph)
+graphStep' g [] _ finalAnts = (finalAnts, g)
+graphStep' lg@(LG g vL eL) (a:as) pheromone finalAnts =
+    let (newAnt, update) = antStep lg a pheromone
+        updatedGraph = LG g vL (updateEdgeLabel update eL)
+    in graphStep' updatedGraph as pheromone (newAnt:finalAnts)
 
+-- Apply the PheromoneUpdate on an edge
 updateEdgeLabel :: PheromoneUpdate -> EdgeLabels -> EdgeLabels
-updateEdgeLabel (PU (u,v) pu) els =
+updateEdgeLabel (PU (u,v) pu) edgeLabels =
     -- need to update the edge in both directions
-    let (Label old) = getEdgeLabel (u,v) els
-        (Label old') = getEdgeLabel (v,u) els
-        els' = setEdgeLabel (u,v) (Label $ old + pu) els
-        in setEdgeLabel (v,u) (Label $ old' + pu) els'
+    let old = getEdgeLabel (u,v) edgeLabels
+        old' = getEdgeLabel (v,u) edgeLabels
+        els' = setEdgeLabel (u,v) (old + pu) edgeLabels
+        in setEdgeLabel (v,u) (old' + pu) els'
 
-initEdgeLabels :: Graph -> Pheromone -> Aco.EdgeLabels
-initEdgeLabels g p = foldr initPheromone M.empty (edges g)
-    where
-        initPheromone edge labels = Aco.setEdgeLabel edge (Label p) labels
+-- Initialize the pheromone on the edges with a fixed value
+initEdgeLabels :: AntGraph -> Pheromone -> AntGraph
+initEdgeLabels (LG g vL eL) phero = LG g vL (foldr initPheromone eL (edges g))
+  where
+    initPheromone edge = setEdgeLabel edge phero
 
-simulate :: Int -> Graph -> [Ant] -> EdgeLabels -> Volatility -> Pheromone -> ([Ant], EdgeLabels)
-simulate  0 _ ants edgeLabels _ _ = (ants, edgeLabels)
-simulate  steps g ants edgeLabels volatility pheromone =
-    let (newAnts, newEdgeLabels) = graphStep g ants (evaporate edgeLabels volatility) pheromone
-    in simulate (steps-1) g newAnts newEdgeLabels volatility pheromone
+-- Evaporate pheromone from all the edges
+evaporate :: EdgeLabels -> Volatility -> EdgeLabels
+evaporate edgeLabels v = M.map (\phero -> phero - (phero * v)) edgeLabels
 
-showEdges :: Show key => EdgeLabels -> Graph -> (Vertex -> (node, key, [key])) -> [String]
-showEdges edgeLabels g f = map printEdge (edges g)
-    where printEdge (u,v) =
-            let (_, u', _) = f u
-                (_, v', _) = f v
-                pheromone = Aco.getEdgeLabel (u,v) edgeLabels
-            in "(" ++ show u' ++ "," ++ show v' ++ ")" ++ ":" ++ show pheromone
+-- Perform the algorithm for a given number of steps
+simulate :: Int -> AntGraph -> [Ant] -> Volatility -> Pheromone -> ([Ant], AntGraph)
+simulate 0 g ants _ _ = (ants, g)
+simulate steps lg ants volatility pheromone =
+    -- let all ants take a step
+    let (newAnts, LG g' vL eL) = graphStep lg ants pheromone
+    -- then evaporate the pheromone
+        newGraph = LG g' vL (evaporate eL volatility)
+    in simulate (steps-1) newGraph newAnts volatility pheromone
+
+-- Pretty print the edges showing the label
+prettyEdges :: Show key => AntGraph -> (Vertex -> (node, key, [key])) -> [String]
+prettyEdges (LG g _ edgeLabels) f = map printEdge (edges g)
+  where
+    printEdge (u,v) =
+        let (_, u', _) = f u
+            (_, v', _) = f v
+            pheromone = Aco.getEdgeLabel (u,v) edgeLabels
+        in "(" ++ show u ++ ":" ++ show u' ++ "->" ++ show v ++ ":" ++ show v' ++ ")" ++ ":" ++ show pheromone
--- app/Main.hs
+++ app/Main.hs
@@ -2,46 +2,97 @@ module Main where
 import qualified Aco
 import System.Random
 import Data.Graph
-import Control.Monad (zipWithM, zipWithM_)
+import Data.Map.Strict as M
+import Control.Monad (zipWithM, replicateM)
 
 
 main :: IO ()
 main = do
-        let steps = 100
-        let volatility = 0.5
-        let depositedPheromone = 1
-        let initPheromone = 1
-        let numAnts = 20
+    -- Configuration variables
+    let steps = 10000
+    let volatility = 0.1
+    let depositedPheromone = 1
+    let initPheromone = 1
+    let numAnts = 20
+    let baseGraph = g1
 
-        {-
-         -      A - B - C
-         -      |   |
-         -      E - D - F
-         -
-         -  Source = A
-         -  Destination = F
-        -}
-        let (graph, nodeFromVertex, vertexFromKey) = graphFromEdges [
-                ("A", 'A', ['B', 'E']),
-                ("B", 'B', ['A', 'C', 'D']),
-                ("C", 'C', ['B']),
-                ("D", 'D', ['B', 'E', 'F']),
-                ("E", 'E', ['D', 'A']),
-                ("F", 'F', ['D'])
-                ]
-        randomGenerators <-  mapM (\_ -> initStdGen) [1..numAnts]
-        let antList = zipWithM (\_ randGen -> makeAnt randGen vertexFromKey 'A' 'F') [1..numAnts] randomGenerators
-        let edgeLabels = Aco.initEdgeLabels graph initPheromone
-        case antList of
-            (Just ants) ->
-                let (_, labels) = Aco.simulate steps graph ants edgeLabels volatility depositedPheromone
-                in mapM_ putStrLn (Aco.showEdges labels graph nodeFromVertex)
-            Nothing -> putStrLn "Something went wrong in antList initialization"
+    -- Initialize ant graph
+    randomGenerators <- replicateM numAnts initStdGen
+    let GP graph nodeFromVertex vertexFromKey srcKey dstKey = baseGraph
+    let antList = zipWithM (\_ randGen -> makeAnt randGen vertexFromKey srcKey dstKey) [1..numAnts] randomGenerators
+    let labeledGraph = Aco.initEdgeLabels (Aco.LG graph M.empty M.empty) initPheromone
+
+    -- Run the algorithm
+    case antList of
+        (Just ants) ->
+            let (endAnts, endGraph) = Aco.simulate steps labeledGraph ants volatility depositedPheromone
+            in do
+                mapM_ putStrLn (Aco.prettyEdges endGraph nodeFromVertex)
+                mapM_ print endAnts
+        Nothing -> putStrLn "Something went wrong in antList initialization"
 
 
 type VertexKey = Char
 makeAnt :: StdGen -> (VertexKey -> Maybe Vertex) -> VertexKey -> VertexKey -> Maybe Aco.Ant
-makeAnt randGen vertexFromKey source dest  =
-        do source' <- vertexFromKey source
-           dest' <- vertexFromKey dest
-           return (Aco.Ant source' Aco.Forward [] source' dest' randGen)
+makeAnt randGen vertexFromKey source dest  = do
+    source' <- vertexFromKey source
+    dest' <- vertexFromKey dest
+    return (Aco.Ant source' Aco.Forward [] source' dest' randGen)
+
+data GraphProblem = GP
+    {
+        gpGraph :: Graph,
+        gpNodeFromVertex :: Vertex -> (String, Char, [Char]),
+        gpVertexFromKey :: Char -> Maybe Vertex,
+        gpSrc :: VertexKey,
+        gpDest :: VertexKey
+    }
+
+{-
+ -      A - B - C
+ -      |   |
+ -      E - D - F
+ -
+ -  Source = A
+ -  Destination = F
+-}
+g1 = GP g nfv vfk 'A' 'F'
+  where
+    (g, nfv, vfk) = graphFromEdges
+        [
+            ("A", 'A', ['B', 'E']),
+            ("B", 'B', ['A', 'C', 'D']),
+            ("C", 'C', ['B']),
+            ("D", 'D', ['B', 'E', 'F']),
+            ("E", 'E', ['D', 'A']),
+            ("F", 'F', ['D'])
+        ]
+
+{-
+ -      A - B
+ -
+ -  Source = A
+ -  Destination = B
+-}
+g2 = GP g nfv vfk 'A' 'B'
+  where
+    (g, nfv, vfk) = graphFromEdges
+        [
+            ("A", 'A', ['B']),
+            ("B", 'B', ['A'])
+        ]
+
+{-
+ -      A - B - C
+ -
+ -  Source = A
+ -  Destination = C
+-}
+g3 = GP g nfv vfk 'A' 'C'
+  where
+    (g, nfv, vfk) = graphFromEdges
+        [
+            ("A", 'A', ['B']),
+            ("B", 'B', ['A','C']),
+            ("C", 'C', ['B'])
+        ]
